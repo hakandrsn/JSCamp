967dfb266909b3cb20cbaa81dafe495ae5058720
Hakan Dursun <hakandrsnn@gmail.com>
2021-10-19 12:51:41 +0300

    JS DERS

---
+++ src/app.js
@@ -0,0 +1,15 @@
+let hakan = "kral"
+console.log(hakan)
+//Array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["konut kredisi" , "Emlak Konut Kredisi" , "Ev Al"]
+
+console.log("<ul>")
+for(let i = 0; i<3;i++){
+console.log("<li>" + konutKredileri[i] + "</li>")
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
